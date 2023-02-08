1c9c074575aa3168aca141c97889e53af90778b8
miriamvalades <miriamex56@gmail.com>
2023-02-08 18:10:20 +0000

    Jest

--- test.js
+++ test.js
@@ -1,16 +1,19 @@
-// importar la función sum del archivo app.js
 const { sum } = require('./app.js');
-
+const { fromEuroToDollar, fromDollarToYen, fromYenToPound } = require('./app.js');
 
 test('adds 14 + 9 to equal 23', () => {
-    let total = sum(14, 9);
-    expect(total).toBe(23);
+let total = sum(14, 9);
+expect(total).toBe(23);
 });
 
 test("One euro should be 1.206 dollars", function(){
-    // importo la funcion desde app.js
-    const { fromEuroToDollar } = require('./app.js')
-    
-    // hago mi comparacion (la prueba)
-    expect(fromEuroToDollar(3.5)).toBe(4.2); //1 euro son 1.2 dolares, entonces 3.5 euros deberian ser = (3.5 * 1.2)
-})
+expect(fromEuroToDollar(3.5)).toBe(4.2);
+});
+
+test("Three euro should be 127.9 yen", function(){
+expect(fromDollarToYen(3)).toBe(383.7);
+});
+
+test("Three euro should be 0.8 pound", function(){
+expect(fromYenToPound(3)).toBe(2.4);
+});
